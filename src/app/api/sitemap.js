766b9183d1b731cdfdc16310c0dbe74f672a51f2
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-02-25 12:42:45 +0530

    seo changes 2

--- src/app/api/sitemap.js
+++ src/app/api/sitemap.js
@@ -1,25 +1,17 @@
-import { NextResponse } from 'next/server';
-
 export async function GET() {
-  const siteUrl = 'https://www.innonsh.com';
-  const pages = ['/', '/about-us', '/contact-us', '/solutions', '/privacy-policy', '/terms-of-services'];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map(
-        (page) => `<url>
-          <loc>${siteUrl}${page}</loc>
-          <changefreq>monthly</changefreq>
-          <priority>0.8</priority>
-        </url>`
-      )
-      .join('')}
-  </urlset>`;
-
-  return new NextResponse(sitemap, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  });
-}
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://www.innonsh.com/</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <priority>1.0</priority>
+      </url>
+    </urlset>`;
+  
+    return new Response(sitemap, {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    });
+  }
+  
